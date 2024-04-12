30515d7dee473ca6503d734d894fa4a7497d51cd
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-04-12 13:16:50 -0700

    added bcrypt

--- models.js
+++ models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -23,6 +25,14 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+//hashing password
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
