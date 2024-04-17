fe1384184bef9c28629bd4e2b229114a2c02edd7
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-04-17 12:49:49 -0700

    temporary connection

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( 'mongodb://localhost:27017/mFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('common'));
 
