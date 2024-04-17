df80429dfb3ff7e0d73b7dddff8431cca9338a36
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-04-17 11:56:50 -0700

    change port

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ app.use((err, req, res, next) => {
 
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.CONNECTION_URI || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
