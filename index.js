afb70286863ea690f2b08698666d4e6d76b7cb6f
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-04-23 10:28:04 -0700

    adding express.json

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to my movie app!");
 });
 
-//app.use(express.json());
+app.use(express.json());
 // serve the “documentation.html” and any other files from the public folder
 app.use(express.static('public'));
 
