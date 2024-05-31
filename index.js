7673957daafda4610d073c4756c6462e9ddbe712
sdncoss <157067345+sdncoss@users.noreply.github.com>
2024-05-31 11:26:16 -0700

    adding cors stipulations

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ const app = express();
 
 //adding CORS
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'https://movieflixappforyou.netlify.app/'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 //Adding auth.js 
 let auth = require('./auth')(app);
